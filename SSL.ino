5589b5eae26515e3fea0cd5d8c62a4826f2bbd9f
DansDansRevolution <60245598+DansDansRevolution@users.noreply.github.com>
2022-06-23 14:56:18 -0600

    Add files via upload

--- SSL.ino
+++ SSL.ino
@@ -1,58 +1,15 @@
-#include <arduinoFFT.h>
-
-#define SAMPLES 128
-#define SAMPLING_FREQUENCY 2048
-
-arduinoFFT FFT = arduinoFFT();
-
-unsigned int samplingPeriod;
-unsigned long microSeconds;
- 
-double vReal[SAMPLES]; //create vector of size SAMPLES to hold real values
-double vImag[SAMPLES]; //create vector of size SAMPLES to hold imaginary values
-
 void setup() {
+  Serial.begin(115200);
   pinMode(A3, INPUT);
   pinMode(A4, INPUT);
   pinMode(A5, INPUT);
-  pinMode(7, OUTPUT);
-  Serial.begin(115200);
-  samplingPeriod = round(1000000*(1.0/SAMPLING_FREQUENCY));
+ 
 }
 
 void loop() {
-  int x = analogRead(A3);
-  int y = analogRead(A4);
-  int z = analogRead(A5);
-  Serial.println(200);
-  Serial.print(" ");
-  Serial.print(600);
-  Serial.print(" ");
-  Serial.print(x);
-  Serial.print(" ");
-  Serial.print(y-400);
-  Serial.print(" ");
-  Serial.print(z);
-  Serial.print(" ");
-
-  for(int i=0; i<SAMPLES; i++)
-    {
-        microSeconds = micros();    //Returns the number of microseconds since the Arduino board began running the current script. 
-     
-        vReal[i] = analogRead(0); //Reads the value from analog pin 0 (A0), quantize it and save it as a real term.
-        vImag[i] = 0; //Makes imaginary term 0 always
-
-        /*remaining wait time between samples if necessary*/
-        while(micros() < (microSeconds + samplingPeriod))
-        {
-          //do nothing
-        }
-    }
-    FFT.Windowing(vReal, SAMPLES, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
-    FFT.Compute(vReal, vImag, SAMPLES, FFT_FORWARD);
-    FFT.ComplexToMagnitude(vReal, vImag, SAMPLES);
-
-    /*Find peak frequency and print peak*/
-    double peak = FFT.MajorPeak(vReal, SAMPLES, SAMPLING_FREQUENCY);
-    Serial.println(peak);     //Print out the most dominant frequency.
+  Serial.print(analogRead(A3)-800);
+  Serial.print(",");
+  Serial.print(analogRead(A4)-800);
+  Serial.print(",");
+  Serial.println(analogRead(A5)-800);
 }
