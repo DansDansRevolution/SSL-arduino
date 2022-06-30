de200b1dacbdf4c8661e43103fb5d1df9403d0ab
DansDansRevolution <60245598+DansDansRevolution@users.noreply.github.com>
2022-06-30 15:02:46 -0600

    Update sampling_rate.ino

--- sampling_rate.ino
+++ sampling_rate.ino
@@ -1,4 +1,10 @@
+int sampleSize = 1024;
+int t2 = 0;
+int t1=0;
+int sumDT = 0;
+int dt = 0;
 
+float micro = 10e-6;
 
 void setup() {
   // put your setup code here, to run once:
@@ -6,23 +12,33 @@ void setup() {
   pinMode(A3, INPUT);
   pinMode(A4, INPUT);
   pinMode(A5, INPUT);
+
+
+  
+  ADC->CTRLB.reg = ADC_CTRLB_PRESCALER_DIV32 |    
+                   ADC_CTRLB_RESSEL_10BIT;         
+
+  ADC->SAMPCTRL.reg = 0x00;
+  
 }
 
 void loop() {
-  float dt = 0;
+  sumDT = 0;
   int n = 0;
-  float t1 = micros();
-  while (n < 1000){
+  t1 = micros();
+  while (n < sampleSize){
     analogRead(A3);
     analogRead(A4);
     analogRead(A5);
+    t2 = micros();
+    dt = t2 - t1;
+    sumDT= sumDT + dt;
+    t1 = t2;
     n++;
   }
-  float t2 = micros();
-  dt = t2 - t1;
-  t1 = t2;
+  
 
-  float avgTime = dt / 1000;
+  int avgTime = sumDT / sampleSize;
   Serial.println(avgTime);
 
 }
